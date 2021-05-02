ffd1babdb74c9f3aa467a83dd8783e250b5904ee
Noah Rahm <correctsyntax@yahoo.com>
2021-05-01 23:04:36 -0500

    update icons build script

--- assets/encode_bitmaps.py
+++ assets/encode_bitmaps.py
@@ -1,13 +1,26 @@
-#!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# Gimel Studio Copyright 2019-2021 by Noah Rahm and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
 
 """
-This is a way to save the startup time when running img2py on lots of
-files...
+Utility script to generate PyEmbeddedImage bitmap icons and write into
+'icons.py' file from images in the icons_source directory.
 
-Add this to the top of the file:
-    from wx.lib.embeddedimage import PyEmbeddedImage
-
-Then you're good to go!
+1. Just run this file in-place and an 'icons.py' file should be
+generated.
+2. Place the icons file in the Gimel Studio 'datafiles' directory.
 """
 
 import os
@@ -36,3 +49,7 @@ if __name__ == "__main__":
         args = line.split()
         img2py.main(args)
 
+    # Add import statement to top
+    with open("icons.py", "r+") as file:
+        file.write("from wx.lib.embeddedimage import PyEmbeddedImage\n#")
+    print("Done!")
