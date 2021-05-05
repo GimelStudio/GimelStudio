66dffd0bc107a496cb8f15a5ed6e7b99e283982e
Noah Rahm <correctsyntax@yahoo.com>
2021-05-05 11:14:17 -0500

    implement imageviewport panel and area

--- src/gimelstudio/interface/imageviewport_pnl.py
+++ src/gimelstudio/interface/imageviewport_pnl.py
@@ -16,15 +16,86 @@
 
 import wx
 import wx.adv
+from wx.lib.newevent import NewCommandEvent
 
 import numpy as np
 
+from gswidgetkit import Button, NumberField, EVT_NUMBERFIELD_CHANGE
+
 from .utils import ConvertImageToWx
 from .basewidgets import ZoomPanel
-from gimelstudio.datafiles import ICON_BRUSH_CHECKERBOARD
+from gimelstudio.datafiles import (ICON_BRUSH_CHECKERBOARD, 
+                                   ICON_IMAGEVIEWPORT_PANEL, 
+                                   ICON_MORE_MENU_SMALL)
+
+imageviewport_mousezoom_cmd_event, EVT_IMAGEVIEWPORT_MOUSEZOOM = NewCommandEvent()
 
 
-class ImageViewportPanel(ZoomPanel):
+
+class ImageViewportPanel(wx.Panel):
+    def __init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize,
+                 style=wx.NO_BORDER | wx.TAB_TRAVERSAL):
+        wx.Panel.__init__(self, parent, id, pos, size, style)
+
+        self._parent = parent
+
+        self.SetBackgroundColour(wx.Colour("#464646"))
+
+        self.BuildUI()
+
+    def BuildUI(self):
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        topbar = wx.Panel(self)
+        topbar.SetBackgroundColour("#424242")
+
+        topbar_sizer = wx.GridBagSizer(vgap=1, hgap=1)
+
+        self.area_icon = wx.StaticBitmap(topbar, bitmap=ICON_IMAGEVIEWPORT_PANEL.GetBitmap())
+        self.area_label = wx.StaticText(topbar, label="Image Viewport")
+        self.area_label.SetForegroundColour("#fff")
+        self.area_label.SetFont(self.area_label.GetFont().Bold())
+
+        self.zoom_field = NumberField(topbar, default_value=100, label="Zoom",
+                                      min_value=25, max_value=250, suffix="%", 
+                                      show_p=False)
+        self.menu_button = Button(topbar, label="", flat=True,
+                                  bmp=(ICON_MORE_MENU_SMALL.GetBitmap(), 'left'))
+
+        topbar_sizer.Add(self.area_icon, (0, 0), flag=wx.LEFT | wx.TOP | wx.BOTTOM, border=8)
+        topbar_sizer.Add(self.area_label, (0, 1), flag=wx.ALL, border=8)
+        topbar_sizer.Add(self.zoom_field, (0, 3), flag=wx.ALL, border=3)
+        topbar_sizer.Add(self.menu_button, (0, 4), flag=wx.ALL, border=3)
+        topbar_sizer.AddGrowableCol(2)
+
+        topbar.SetSizer(topbar_sizer)
+
+        self.imageviewport = ImageViewport(self)
+
+        main_sizer.Add(topbar, flag=wx.EXPAND | wx.LEFT | wx.RIGHT)
+        main_sizer.Add(self.imageviewport, 1, flag=wx.EXPAND | wx.BOTH)
+
+        self.SetSizer(main_sizer)
+
+        self.imageviewport.Bind(EVT_IMAGEVIEWPORT_MOUSEZOOM, self.ZoomImageViewport)
+        self.zoom_field.Bind(EVT_NUMBERFIELD_CHANGE, self.ChangeZoom)
+
+    def UpdateViewerImage(self, image, render_time):
+        self.imageviewport.UpdateViewerImage(image, render_time)
+
+    def ChangeZoom(self, event):
+        level = event.value / 100.0
+        self.imageviewport.SetZoomLevel(level)
+
+    def ZoomImageViewport(self, event):
+        self.zoom_field.SetValue(event.value)
+        self.zoom_field.UpdateDrawing()
+        self.imageviewport.UpdateDrawing()
+        self.zoom_field.Refresh()
+        self.Refresh()
+
+
+class ImageViewport(ZoomPanel):
     def __init__(self, parent):
         ZoomPanel.__init__(self, parent)
 
@@ -62,13 +133,6 @@ class ImageViewportPanel(ZoomPanel):
     def OnDrawInterface(self, dc):
         gc = wx.GraphicsContext.Create(dc)
 
-        self.UpdateZoomValue()
-        text = "Zoom {0}%".format(self._zoom)
-
-        fnt = self._parent.GetFont().Bold()
-        gc.SetFont(fnt, wx.Colour('#ccc'))
-        gc.DrawText(text, 10, self.Size[1]-30)
-
     def OnKeyEvent(self, event):
         code = event.GetKeyCode()
         mouse = wx.Point(self.Size[0] / 2, self.Size[1] / 2)
@@ -80,10 +144,37 @@ class ImageViewportPanel(ZoomPanel):
         elif code == wx.WXK_NUMPAD_SUBTRACT:
             self.ScenePostScale(0.9, 0.9, mouse[0], mouse[1])
 
+        self.UpdateZoomValue()
+        self.SendMouseZoomEvent()
+        self.UpdateDrawing()
+
+    def OnMousewheel(self, event):
+        rotation = event.GetWheelRotation()
+        mouse = event.GetPosition()
+        if rotation > 1:
+            self.ScenePostScale(1.1, 1.1, mouse[0], mouse[1])
+        elif rotation < -1:
+            self.ScenePostScale(0.9, 0.9, mouse[0], mouse[1])
+
+        self.UpdateZoomValue()
+        self.SendMouseZoomEvent()
         self.UpdateDrawing()
 
     def UpdateZoomValue(self):
         self._zoom = round(self.GetScaleX() * 100)
+
+    def SetZoomLevel(self, zoom, x=0, y=0):
+        if x == 0:
+            x = self.Size[0]/2
+            y = self.Size[1]/2
+        self.ScenePostScale(zoom, zoom, x, y)
+        self.UpdateZoomValue()
+        self.UpdateDrawing()
+
+    def SendMouseZoomEvent(self):
+        wx.PostEvent(self, 
+                     imageviewport_mousezoom_cmd_event(id=self.GetId(), 
+                     value=self._zoom))
 
     def UpdateViewerImage(self, image, render_time):
         """ Update the Image Viewport. This refreshes everything
