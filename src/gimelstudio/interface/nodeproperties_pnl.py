81651e38a89c10ff70fd6caf5ee739bfdfefbf0c
Noah Rahm <correctsyntax@yahoo.com>
2021-05-05 12:33:06 -0500

    implement nodeproperties panel and area

--- src/gimelstudio/interface/nodeproperties_pnl.py
+++ src/gimelstudio/interface/nodeproperties_pnl.py
@@ -18,6 +18,12 @@ import wx
 import wx.html
 import wx.lib.agw.foldpanelbar as fpb
 
+from gswidgetkit import Button
+
+from gimelstudio.datafiles import (ICON_NODEPROPERTIES_PANEL, 
+                                   ICON_MORE_MENU_SMALL,
+                                   ICON_HELP)
+
 
 class NodePropertiesPanel(wx.Panel):
     def __init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize,
@@ -25,14 +31,10 @@ class NodePropertiesPanel(wx.Panel):
         wx.Panel.__init__(self, parent, id, pos, size, style)
 
         self._parent = parent
-        self._selectedNode = None
-
-        self._mainSizer = wx.BoxSizer(wx.VERTICAL)
-
-        self.SetSizer(self._mainSizer)
-        self._mainSizer.Fit(self)
 
         self.SetBackgroundColour(wx.Colour("#464646"))
+
+        self.BuildUI()
 
     @property
     def Parent(self):
@@ -42,17 +44,70 @@ class NodePropertiesPanel(wx.Panel):
     def AUIManager(self):
         return self._parent._mgr
 
+    def BuildUI(self):
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        topbar = wx.Panel(self)
+        topbar.SetBackgroundColour("#424242")
+
+        topbar_sizer = wx.GridBagSizer(vgap=1, hgap=1)
+
+        self.area_icon = wx.StaticBitmap(topbar, bitmap=ICON_NODEPROPERTIES_PANEL.GetBitmap())
+        self.area_label = wx.StaticText(topbar, label="")
+        self.area_label.SetForegroundColour("#fff")
+        self.area_label.SetFont(self.area_label.GetFont().Bold())
+
+        self.menu_button = Button(topbar, label="", flat=True,
+                                  bmp=(ICON_MORE_MENU_SMALL.GetBitmap(), 'left'))
+
+        topbar_sizer.Add(self.area_icon, (0, 0), flag=wx.LEFT | wx.TOP | wx.BOTTOM, border=8)
+        topbar_sizer.Add(self.area_label, (0, 1), flag=wx.ALL, border=8)
+        topbar_sizer.Add(self.menu_button, (0, 4), flag=wx.ALL, border=3)
+        topbar_sizer.AddGrowableCol(2)
+
+        topbar.SetSizer(topbar_sizer)
+
+        self.main_panel = wx.Panel(self)
+        self._mainsizer = wx.BoxSizer(wx.VERTICAL)
+        self.main_panel.SetSizer(self._mainsizer)
+        self._mainsizer.Fit(self.main_panel)
+
+        main_sizer.Add(topbar, flag=wx.EXPAND | wx.LEFT | wx.RIGHT)
+        main_sizer.Add(self.main_panel, 1, flag=wx.EXPAND | wx.BOTH)
+
+        self.SetSizer(main_sizer)
+
     def UpdatePanelContents(self, selected_node):
-        self.DestroyChildren()
+        self.main_panel.DestroyChildren()
 
         if selected_node is not None:
+                     
+            # Node info
+            nodeinfo_pnl = wx.Panel(self.main_panel, size=(-1, 50))
+            nodeinfo_pnl.SetBackgroundColour("#464646")
 
-            panel_bar = fpb.FoldPanelBar(self, agwStyle=fpb.FPB_VERTICAL)
+            nodeinfo_pnl_sizer = wx.GridBagSizer(vgap=1, hgap=1)
 
-            # Node Properties UI
-            selected_node.NodePanelUI(self, panel_bar)
+            node_label = wx.StaticText(nodeinfo_pnl, label=selected_node._label)
+            node_label.SetForegroundColour("#fff")
+            node_label.SetFont(self.area_label.GetFont().Bold())
 
-            self._mainSizer.Add(panel_bar, 1, wx.EXPAND | wx.ALL)
+            self.help_button = Button(nodeinfo_pnl, label="", flat=True,
+                                      bmp=(ICON_HELP.GetBitmap(), 'left'))
+
+            nodeinfo_pnl_sizer.Add(node_label, (0, 1), 
+                                   flag=wx.TOP | wx.BOTTOM | wx.LEFT, border=6)
+            nodeinfo_pnl_sizer.Add(self.help_button, (0, 4), 
+                                   flag=wx.TOP | wx.BOTTOM | wx.RIGHT, border=6)
+            nodeinfo_pnl_sizer.AddGrowableCol(2)
+            nodeinfo_pnl.SetSizer(nodeinfo_pnl_sizer)
+
+            self._mainsizer.Add(nodeinfo_pnl, flag=wx.EXPAND)
+
+            # Node Properties
+            panel_bar = fpb.FoldPanelBar(self.main_panel, agwStyle=fpb.FPB_VERTICAL)
+
+            selected_node.NodePanelUI(self.main_panel, panel_bar)
             
             style = fpb.CaptionBarStyle()
             style.SetCaptionFont(self.Parent.GetFont())
@@ -61,11 +116,13 @@ class NodePropertiesPanel(wx.Panel):
             style.SetSecondColour(wx.Colour('#5c5c5c'))
             panel_bar.ApplyCaptionStyleAll(style)
 
-            self._mainSizer.Layout()
+            self._mainsizer.Add(panel_bar, 1, wx.EXPAND | wx.ALL)
+
+            self._mainsizer.Layout()
 
         else:
             # Delete the window if the node is not selected
-            self._mainSizer.Clear(delete_windows=True)
+            self._mainsizer.Clear(delete_windows=True)
 
         self.AUIManager.Update()
         self.Parent.Refresh()
