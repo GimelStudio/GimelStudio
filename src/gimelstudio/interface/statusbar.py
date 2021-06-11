f3ecb9855e37b3eede4e2eeb5e0876bc0f489206
Noah Rahm <correctsyntax@yahoo.com>
2021-06-10 23:23:53 -0500

    correct header copyright message in statusbar.py

--- src/gimelstudio/interface/statusbar.py
+++ src/gimelstudio/interface/statusbar.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# GS Widget Kit Copyright 2021 by Noah Rahm and contributors
+# Gimel Studio Copyright 2019-2021 by Noah Rahm and contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
