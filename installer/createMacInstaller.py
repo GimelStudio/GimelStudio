b7c0cc11695401b62e09692f5109b22ef2ca2219
yasuomaidana <yasuomaidana@gmail.com>
2021-06-19 02:29:27 -0500

    Make installer to work in the same directory

--- installer/createMacInstaller.py
+++ installer/createMacInstaller.py
@@ -1,49 +1,65 @@
 import os
 import sys
-def execS(inst,env='source env/bin/activate && '):
+if getattr(sys, 'frozen', False):
+    rootP = os.path.dirname(sys.executable)+'/'
+elif __file__:
+    rootP = os.path.dirname('')
+print(rootP)
+
+def execS(inst,env='source {}env/bin/activate && '.format(rootP)):
     terminalInstruction = env+inst
     os.system(terminalInstruction)
     
-def checkPathandExecute(path,inst):
-    if not os.path.exists(path):
+def checkPathandExecute(path,inst,g=False):
+    if g:
+        ar=path
+    else:
+        ar=rootP+path
+    if not os.path.exists(ar):
         execS(inst,'')
 def remDir(dir):
-    return "rm -rf {}".format(dir)
-    
+    return "rm -rf {}".format(rootP+dir)
+def rem(fd,d="f"):
+    execS("rm -"+d+" "+rootP+fd,'')
+
 def getFiles():
     dirFile=["assets","requirements.txt","src"]
     for file in dirFile:
-        checkPathandExecute(file,"git clone https://github.com/GimelStudio/GimelStudio gs")
+        checkPathandExecute(file,"git clone https://github.com/GimelStudio/GimelStudio {}gs".format(rootP))
         break
     for file in dirFile:
-        execS("mv gs/{f} {f}".format(f=file),"")
-    execS(remDir('gs'),'')
+        execS("mv {r}gs/{f} {r}{f}".format(f=file,r=rootP),"")
+    rem('gs',"rf")
+
+def cleanFiles():
+    rem('env','rf')
+    rem('build','rf')
+    execS('mv {r}dist/GimelStudio.app {r}GimelStudio.app'.format(r=rootP),'')
+    rem('dist','rf')
+    rem('*.spec')
+    rem('assets','rf')
+    rem('requirements.txt')
+    rem('src','rf')
+
+
 
 getFiles()
-checkPathandExecute('env','python3 -m venv --system-site-packages env')
-checkPathandExecute('/usr/local/Cellar/openimageio','brew install openimageio')
-def cleanFiles():
-    if getattr(sys, 'frozen', False):
-        application_path = os.path.dirname(sys.executable)
-    elif __file__:
-        application_path = os.path.dirname('')
-    execS('rm -rf env','')
-    execS('rm -rf build','')
-    execS('mv dist/GimelStudio.app ' +application_path+'/GimelStudio.app','')
-    execS('rm -rf dist','')
-    execS('rm -rf *.spec','')
-    execS('rm -rf assets','')
-    execS('rm -f requirements.txt','')
-    execS('rm -rf src','')
+checkPathandExecute('env','python3 -m venv --system-site-packages {}env'.format(rootP))
+checkPathandExecute('/usr/local/Cellar/openimageio','brew install openimageio',True)
+
     
     
 #Activating virtual enviroment
 execS("pip3 install pyinstaller")
-inst='pyinstaller --onefile src/main.py'
-for lib in open('requirements.txt'):
+inst='pyinstaller --onefile {}src/main.py'.format(rootP)
+for lib in open('{}requirements.txt'.format(rootP)):
     inst+=' --hidden-import {}'.format(lib.replace('\n',''))
-inst+=' --hidden-import wx --windowed -i assets/GIMELSTUDIO_ICO.ico -n GimelStudio'
+inst+=' --hidden-import wx --windowed -i {}assets/GIMELSTUDIO_ICO.ico -n GimelStudio'.format(rootP)
 inst+=' --noconfirm'
+inst+=' --distpath {}dist'.format(rootP)
+inst+=' --workpath {}build'.format(rootP)
+inst+=' --specpath {}'.format(rootP)
+
 execS(inst)
 cleanFiles()
 
