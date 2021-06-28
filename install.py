75f6268878a8e16e3b7d43a96693a58b3f239a49
Noah <correctsyntax@yahoo.com>
2021-06-28 09:06:08 -0500

    delete install.py

--- install.py
+++
@@ -1,7 +0,0 @@
-from sys import platform
-if platform == "linux" or platform == "linux2":
-    pass
-elif platform == "darwin":
-    import installer.createMacInstaller
-elif platform == "win32":
-    pass
