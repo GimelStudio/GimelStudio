f7a0e88fff8da77d1aeae5c66e688d0ab7eea8a8
Noah Rahm <correctsyntax@yahoo.com>
2021-11-23 18:15:46 -0600

    Use XYZProp for checkered node offset value

--- src/nodescripts/corenodes/input/checkered_image_node.py
+++ src/nodescripts/corenodes/input/checkered_image_node.py
@@ -32,7 +32,7 @@ class CheckeredImageNode(api.Node):
         meta_info = {
             "label": "Checkered",
             "author": "Gimel Studio",
-            "version": (0, 0, 5),
+            "version": (0, 1, 0),
             "category": "INPUT",
             "description": "Creates a checkered image."
         }
@@ -52,21 +52,36 @@ class CheckeredImageNode(api.Node):
     def NodeInitProps(self):
         self.checker_size = api.PositiveIntegerProp(
             idname="checker_size",
-            default=44,
+            default=46,
             min_val=0,
             max_val=100,
             label="Checker Size"
         )
+        self.offset = api.XYZProp(
+            idname="offset", 
+            default=(0, 0, 0), 
+            labels=("X", "Y", "Z"),
+            min_vals=(0, 0, 0), 
+            max_vals=(200, 200, 200),
+            show_p=False, 
+            enable_z=True,
+            label="Offset"
+        )
 
         self.NodeAddProp(self.checker_size)
+        self.NodeAddProp(self.offset)
 
     def NodeEvaluation(self, eval_info):
         checker_size = self.EvalProperty(eval_info, "checker_size")
+        offset = self.EvalProperty(eval_info, "offset")
 
         render_image = api.RenderImage()
 
         buf = oiio.ImageBuf(oiio.ImageSpec(1200, 1200, 3, oiio.UINT16))
-        oiio.ImageBufAlgo.checker(buf, checker_size, checker_size, 1, (0.1,0.1,0.1), (0.4,0.4,0.4))
+        oiio.ImageBufAlgo.checker(buf, checker_size, checker_size, 1, 
+                                  color1=(0.1,0.1,0.1), color2=(0.4,0.4,0.4),
+                                  xoffset=offset[0], yoffset=offset[1], 
+                                  zoffset=offset[2])
 
         render_image.SetAsImage(buf)
         self.NodeUpdateThumb(render_image)
