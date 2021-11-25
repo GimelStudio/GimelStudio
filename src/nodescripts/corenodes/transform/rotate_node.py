b504f2de678e359cf4251881caf37f99e25da347
Noah Rahm <correctsyntax@yahoo.com>
2021-11-24 22:25:30 -0600

    Add Rotate node

---
+++ src/nodescripts/corenodes/transform/rotate_node.py
@@ -0,0 +1,73 @@
+# ----------------------------------------------------------------------------
+# Gimel Studio Copyright 2019-2021 by Noah Rahm and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+
+import math
+try:
+    import OpenImageIO as oiio
+except ImportError:
+    print("""OpenImageIO is required! Get the python wheel for Windows at:
+     https://www.lfd.uci.edu/~gohlke/pythonlibs/#openimageio""")
+
+from gimelstudio import api
+
+
+class RotateNode(api.Node):
+    def __init__(self, nodegraph, _id):
+        api.Node.__init__(self, nodegraph, _id)
+
+    @property
+    def NodeMeta(self):
+        meta_info = {
+            "label": "Rotate",
+            "author": "Gimel Studio",
+            "version": (0, 5, 0),
+            "category": "TRANSFORM",
+            "description": "Rotates an image.",
+        }
+        return meta_info
+
+    def NodeInitProps(self):
+        self.rotation = api.PositiveIntegerProp(
+            idname="rotation",
+            default=90,
+            min_val=0,
+            max_val=360,
+            lbl_suffix="°",
+            label="Rotation"
+        )
+
+        self.NodeAddProp(self.rotation)
+
+    def NodeInitParams(self):
+        image = api.RenderImageParam("image", "Image")
+
+        self.NodeAddParam(image)
+
+    def NodeEvaluation(self, eval_info):
+        rotation = self.EvalProperty(eval_info, "rotation")
+        image1 = self.EvalParameter(eval_info, "image")
+
+        render_image = api.RenderImage()
+        img = image1.Image("oiio")
+
+        output_img = oiio.ImageBufAlgo.rotate(img, math.radians(rotation))
+
+        render_image.SetAsImage(output_img)
+        self.NodeUpdateThumb(render_image)
+        return render_image
+
+
+api.RegisterNode(RotateNode, "corenode_rotate")
