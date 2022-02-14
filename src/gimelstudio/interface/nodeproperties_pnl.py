75398ac5d7f0ca407e5cb5bd8d01966c73c0b4ee
Noah Rahm <correctsyntax@yahoo.com>
2022-02-14 14:11:07 -0600

    Optimize and cleanup properties panel

--- src/gimelstudio/interface/nodeproperties_pnl.py
+++ src/gimelstudio/interface/nodeproperties_pnl.py
@@ -21,11 +21,34 @@ from gswidgetkit import (EVT_BUTTON, Button, Label)
 from gimelstudio.constants import (PROP_BG_COLOR, AREA_BG_COLOR, 
                                    AREA_TOPBAR_COLOR, TEXT_COLOR)
 from gimelstudio.datafiles import (ICON_HELP, ICON_NODEPROPERTIES_PANEL,
-                                   ICON_MORE_MENU_SMALL, ICON_MOUSE_LMB,
-                                   ICON_MOUSE_MMB, ICON_MOUSE_RMB)
+                                   ICON_MORE_MENU_SMALL)
 #from gimelstudio.core.node.property import ThumbProp
 from .message_dlgs import ShowNotImplementedDialog
 from .panel_base import PanelBase
+
+
+class NodeInfoPanel(wx.Panel):
+    def __init__(self, parent, *args, **kwds):
+        wx.Panel.__init__(self, parent, *args, **kwds)
+
+        self.SetBackgroundColour(AREA_BG_COLOR)
+
+        nodeinfo_pnl_sizer = wx.GridBagSizer(vgap=1, hgap=1)
+
+        self.node_label = Label(self, label="")
+        self.help_button = Button(self, label="", flat=True, bmp=(ICON_HELP.GetBitmap(), 'left'))
+
+        nodeinfo_pnl_sizer.Add(self.node_label, (0, 1),
+                                flag=wx.TOP | wx.BOTTOM, border=10)
+        nodeinfo_pnl_sizer.Add(self.help_button, (0, 4),
+                                flag=wx.TOP | wx.BOTTOM | wx.RIGHT, border=10)
+        nodeinfo_pnl_sizer.AddGrowableCol(2)
+        self.SetSizer(nodeinfo_pnl_sizer)
+
+        self.help_button.Bind(EVT_BUTTON, self.OnHelpButton)
+
+    def OnHelpButton(self, event):
+        ShowNotImplementedDialog()
 
 
 class NodePropertiesPanel(PanelBase):
@@ -35,14 +58,12 @@ class NodePropertiesPanel(PanelBase):
         self.thumb_pnl_expanded = False
 
         self.caption_style = fpb.CaptionBarStyle()
-        self.caption_style.SetCaptionFont(self.parent.GetFont())
         self.caption_style.SetCaptionColour(wx.Colour(TEXT_COLOR))
         self.caption_style.SetFirstColour(wx.Colour(PROP_BG_COLOR))
         self.caption_style.SetCaptionStyle(fpb.CAPTIONBAR_SINGLE)
 
-        self.SetBackgroundColour(AREA_BG_COLOR)
-
         self.BuildUI()
+        self.SetBackgroundColour(AREA_BG_COLOR)
 
     @property
     def Parent(self):
@@ -57,8 +78,9 @@ class NodePropertiesPanel(PanelBase):
         return self.parent.statusbar
 
     def BuildUI(self):
-        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        primary_sizer = wx.BoxSizer(wx.VERTICAL)
 
+        # Topbar
         topbar = wx.Panel(self)
         topbar.SetBackgroundColour(AREA_TOPBAR_COLOR)
 
@@ -66,80 +88,64 @@ class NodePropertiesPanel(PanelBase):
 
         self.area_icon = wx.StaticBitmap(topbar,
                                          bitmap=ICON_NODEPROPERTIES_PANEL.GetBitmap())
-        self.area_label = Label(topbar, label="", color="#ccc", font_bold=True)
+        self.area_label = Label(topbar, label="", color="#ccc", font_bold=False)
 
         self.menu_button = Button(topbar, label="", flat=True,
                                   bmp=(ICON_MORE_MENU_SMALL.GetBitmap(), 'left'))
 
-        topbar_sizer.Add(self.area_icon, (0, 0), flag=wx.LEFT | wx.TOP | wx.BOTTOM,
-                         border=8)
+        topbar_sizer.Add(self.area_icon, (0, 0), flag=wx.LEFT | wx.TOP | wx.BOTTOM, border=8)
         topbar_sizer.Add(self.area_label, (0, 1), flag=wx.ALL, border=8)
         topbar_sizer.Add(self.menu_button, (0, 4), flag=wx.ALL, border=3)
         topbar_sizer.AddGrowableCol(2)
-
         topbar.SetSizer(topbar_sizer)
 
-        self.main_panel = wx.Panel(self)
-        self._mainsizer = wx.BoxSizer(wx.VERTICAL)
-        self.main_panel.SetSizer(self._mainsizer)
-        self._mainsizer.Fit(self.main_panel)
+        # Main panel
+        main_panel = wx.Panel(self)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_panel.SetSizer(main_sizer)
 
-        main_sizer.Add(topbar, flag=wx.EXPAND | wx.LEFT | wx.RIGHT)
-        main_sizer.Add(self.main_panel, 1, flag=wx.EXPAND | wx.BOTH)
+        # Node info
+        self.nodeinfo_pnl = NodeInfoPanel(main_panel)
 
-        self.SetSizer(main_sizer)
+        # Panel where the Properties will be placed
+        self.props_panel = wx.Panel(main_panel)
+        self.props_panel_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.props_panel.SetSizer(self.props_panel_sizer)
+
+        main_sizer.Add(self.nodeinfo_pnl, flag=wx.EXPAND | wx.BOTH)
+        main_sizer.Add(self.props_panel, 1, flag=wx.EXPAND | wx.BOTH)
+
+        primary_sizer.Add(topbar, flag=wx.EXPAND | wx.LEFT | wx.RIGHT)
+        primary_sizer.Add(main_panel, 1, flag=wx.EXPAND | wx.BOTH)
+
+        self.SetSizer(primary_sizer)
 
         self.menu_button.Bind(EVT_BUTTON, self.OnAreaMenuButton)
 
     def UpdatePanelContents(self, selected_node):
-        # Destroy the current panels and freeze to prevent flicker
-        self.main_panel.DestroyChildren()
+        # Destroy the current panels and freeze to prevent glitching
+        self.props_panel.DestroyChildren()
         self.Freeze()
 
         if selected_node is not None:
-
-            # Node info
-            nodeinfo_pnl = wx.Panel(self.main_panel, size=(-1, 50))
-            nodeinfo_pnl.SetBackgroundColour(AREA_BG_COLOR)
-
-            nodeinfo_pnl_sizer = wx.GridBagSizer(vgap=1, hgap=1)
-
-            node_label = Label(nodeinfo_pnl, label=selected_node.GetLabel())
-
-            self.help_button = Button(nodeinfo_pnl, label="", flat=True,
-                                      bmp=(ICON_HELP.GetBitmap(), 'left'))
-
-            nodeinfo_pnl_sizer.Add(node_label, (0, 1),
-                                   flag=wx.TOP | wx.BOTTOM, border=10)
-            nodeinfo_pnl_sizer.Add(self.help_button, (0, 4),
-                                   flag=wx.TOP | wx.BOTTOM | wx.RIGHT, border=10)
-            nodeinfo_pnl_sizer.AddGrowableCol(2)
-            nodeinfo_pnl.SetSizer(nodeinfo_pnl_sizer)
-
-            self._mainsizer.Add(nodeinfo_pnl, flag=wx.EXPAND)
+            self.nodeinfo_pnl.node_label.SetLabel(selected_node.GetLabel())
 
             # Node Properties
-            panel_bar = fpb.FoldPanelBar(self.main_panel, agwStyle=fpb.FPB_VERTICAL)
+            panel_bar = fpb.FoldPanelBar(self.props_panel, agwStyle=fpb.FPB_VERTICAL)
 
-            selected_node.NodePanelUI(self.main_panel, panel_bar)
-            self.CreateThumbPanel(selected_node, self.main_panel, panel_bar)
-
+            selected_node.NodePanelUI(self.props_panel, panel_bar)
+            self.CreateThumbPanel(selected_node, self.props_panel, panel_bar)
             panel_bar.ApplyCaptionStyleAll(self.caption_style)
 
-            self._mainsizer.Add(panel_bar, 1, wx.EXPAND | wx.LEFT | wx.RIGHT, border=6)
-
-            self.help_button.Bind(EVT_BUTTON, self.OnHelpButton)
-
+            self.props_panel_sizer.Add(panel_bar, 1, wx.EXPAND | wx.LEFT | wx.RIGHT, border=6)
+            self.props_panel_sizer.Fit(self.props_panel)
         else:
             # Delete the window if the node is not selected
-            self._mainsizer.Clear(delete_windows=True)
+            self.props_panel.Clear(delete_windows=True)
 
         # Update everything then allow refreshing
         self.AUIManager.Update()
         self.Thaw()
-
-    def OnHelpButton(self, event):
-        ShowNotImplementedDialog()
 
     def CreateThumbPanel(self, node, panel, panel_bar):
         pass
