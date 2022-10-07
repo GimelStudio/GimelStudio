9ff4ff32d9de9db491d079a060bb14d8668b1ccf
I Woithe <iwoithe@just42.net>
2022-10-07 11:09:48 +1030

    Add a todo

--- src/framework/ui/uimodule.cpp
+++ src/framework/ui/uimodule.cpp
@@ -14,6 +14,7 @@ std::string UiModule::moduleName() const
 void UiModule::registerUiTypes()
 {
     UiTheme* uitheme = new UiTheme();
+    // TODO: init() should probably be called in the UiTheme constructor
     uitheme->init();
     // TODO: Is ui.theme a better accesser in QML?
     qmlRegisterSingletonInstance<UiTheme>("GimelStudio.Ui", 1, 0, "UiTheme", uitheme);
