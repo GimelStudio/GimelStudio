d98121fd3732e4e706ed7c058f23ae329f674b29
I Woithe <iwoithe@just42.net>
2022-12-07 15:38:11 +1030

    Split some code into a header file

--- share/nodes/flipnode-exampleapi.cpp
+++ share/nodes/flipnode-exampleapi.cpp
@@ -1,26 +1,9 @@
-// Use this for the built-in nodes
-#include "uicomponents/view/node.h"
+#include "flipnode-examplesapi.h"
 
-// Use this instead for thirdparty nodes
-// #include "gimelstudio/node.h"
+#include "uicomponents/view/properties.h"
+// #include "gimelstudio/properties.h"
 
 #include <QVersionNumber>
-
-using namespace gs::uicomponents;
-// using namespace gs::api;
-
-class FlipNode : public Node
-{
-public:
-    explicit FlipNode();
-    ~FlipNode() override = default;
-
-    QVariantMap metaData() override;
-    void initInputProperties() override;
-    void initOutputProperties() override;
-    void mutedEvaluation() override;
-    void evaluation() override;
-}
 
 FlipNode::FlipNode() : Node {}
 
