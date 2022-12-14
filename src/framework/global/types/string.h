398974a8a9d06088331aa4ff117e17e8faf00ac1
I Woithe <iwoithe@just42.net>
2022-12-15 09:38:06 +1030

    Add the string type

---
+++ src/framework/global/types/string.h
@@ -0,0 +1,12 @@
+#ifndef GS_GLOBAL_TYPES_STRING_H
+#define GS_GLOBAL_TYPES_STRING_H
+
+#include <string>
+
+namespace gs
+{
+// Until a custom string type is made
+using String = std::string;
+}
+
+#endif // GS_GLOBAL_TYPES_STRING_H
