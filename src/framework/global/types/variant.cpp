027a8fdff6113df79d6866f815174504cfb45cec
I Woithe <iwoithe@just42.net>
2022-12-15 10:28:39 +1030

    Add variant.cpp

---
+++ src/framework/global/types/variant.cpp
@@ -0,0 +1,74 @@
+#include "variant.h"
+
+using namespace gs;
+
+Variant Variant::fromValue(VariantType val)
+{
+    Variant v = Variant();
+    v.setValue(val);
+    return v;
+}
+
+template<typename T> Variant Variant::fromValue(T val)
+{
+    Variant v = Variant();
+    v.setValue(val);
+    return v;
+}
+
+template<typename T> T Variant::value()
+{
+    return std::get<T>(m_variant);
+}
+
+template<typename T> T Variant::valueToType()
+{
+    return static_cast<T>(toIType());
+}
+
+void Variant::setValue(Variant val)
+{
+    m_variant = val.variant();
+}
+
+void Variant::setValue(VariantType val)
+{
+    m_variant = val;
+}
+
+double Variant::toDouble()
+{
+    return std::get<double>(m_variant);
+}
+
+float Variant::toFloat()
+{
+    return std::get<float>(m_variant);
+}
+
+int Variant::toInt()
+{
+    return std::get<int>(m_variant);
+}
+
+IType* Variant::toIType()
+{
+    return std::get<IType*>(m_variant);
+}
+
+#ifdef QT_SUPPORT
+template<typename T> QVariant Variant::toQVariant()
+{
+    return QVariant::fromValue<T>(m_variant);
+}
+#endif
+
+std::string Variant::toString()
+{
+    return std::get<std::string>(m_variant);
+}
+
+VariantType Variant::variant() const
+{
+    return m_variant;
+}
--- src/framework/global/types/variant.h
+++ src/framework/global/types/variant.h
@@ -22,33 +22,24 @@ class Variant
 private:
     VariantType m_variant;
 public:
-    static Variant fromValue(VariantType val)
-    {
-        Variant v = Variant();
-        v.setValue(val);
-        return v;
-    }
-    template<typename T> static Variant fromValue(T val)
-    {
-        Variant v = Variant();
-        v.setValue(val);
-        return v;
-    }
-    template<typename T> T value() { return std::get<T>(m_variant); }
-    template<typename T> T valueToType() { return static_cast<T>(toIType()); }
-    void setValue(Variant val) { m_variant = val.variant(); };
-    void setValue(VariantType val) { m_variant = val; };
+    static Variant fromValue(VariantType val);
+    template<typename T> static Variant fromValue(T val);
 
-    double toDouble() { return std::get<double>(m_variant); }
-    float toFloat() { return std::get<float>(m_variant); }
-    int toInt() { return std::get<int>(m_variant); }
-    IType* toIType() { return std::get<IType*>(m_variant); }
+    template<typename T> T value();
+    template<typename T> T valueToType();
+    void setValue(Variant val);
+    void setValue(VariantType val);
+
+    double toDouble();
+    float toFloat();
+    int toInt();
+    IType* toIType();
 #ifdef QT_SUPPORT
     // Unfortunately, a template has to be used here. Otherwise Qt has a massive hissy fit
-    template<typename T> QVariant toQVariant() { return QVariant::fromValue<T>(m_variant); }
+    template<typename T> QVariant toQVariant();
 #endif
-    std::string toString() { return std::get<std::string>(m_variant); }
-    VariantType variant() const { return m_variant; }
+    std::string toString()
+    VariantType variant();
 };
 }
 
