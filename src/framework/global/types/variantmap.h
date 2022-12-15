f672a7aaa0287ff330af688589a738e5e6f40339
I Woithe <iwoithe@just42.net>
2022-12-15 11:11:28 +1030

    Add the variant map type

---
+++ src/framework/global/types/variantmap.h
@@ -0,0 +1,15 @@
+#ifndef GS_GLOBAL_TYPES_VARIANTMAP_H
+#define GS_GLOBAL_TYPES_VARIANTMAP_H
+
+#include <map>
+
+#include "string.h"
+#include "variant.h"
+
+namespace gs
+{
+// Use the standard library map for now (not even sure if a custom class should be created for this)
+using VariantMap = std::map<String, Variant>;
+}
+
+#endif // GS_GLOBAL_TYPES_VARIANTMAP_H
