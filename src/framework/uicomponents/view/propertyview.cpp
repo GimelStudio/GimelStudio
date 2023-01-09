3c73bd3f7c5e9f01b52fe6c7dd51586713fad193
I Woithe <iwoithe@just42.net>
2023-01-09 13:21:43 +1030

    Prepare adding the nodes UI

---
+++ src/framework/uicomponents/view/propertyview.cpp
@@ -0,0 +1,1 @@
+#include "propertyview.h"
--- src/properties/integerproperty.h
+++ src/properties/integerproperty.h
@@ -3,6 +3,8 @@
 
 #include "nodes/iproperty.h"
 
+namespace gs::properties {
 class IntegerProperty : public IProperty {};
+} // gs::properties
 
 #endif // GS_PROPERTIES_INTEGERPROPERTY_H
