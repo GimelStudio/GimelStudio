f55768c03f2c90589a8289a1a6ae73e9d3765041
Correct-Syntax <correctsyntax@yahoo.com>
2025-02-02 21:55:45 +0000

    Build (8e8b855) Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3086924425"
+    serviceWorkerVersion: "4195488749"
   }
 });
