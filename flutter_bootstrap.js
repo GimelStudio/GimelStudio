fc4a3547a8fb63dec2f1d70c1cb18bd02443fd98
Correct-Syntax <correctsyntax@yahoo.com>
2025-02-27 17:48:42 +0000

    Build (cc2fd5b) Update build.yml

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "979830560"
+    serviceWorkerVersion: "1520701254"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "da967fad356643acf318c9066c0bd487",
 "manifest.json": "075a4a02f9b9da38f5f0f2ebaf17ad7f",
 "favicon.png": "85090bb4c150a1101805df086e708563",
-"flutter_bootstrap.js": "60b6c2d4c9e20cdc4dc69ecdc044f0aa"};
+"flutter_bootstrap.js": "b0ab31ce0372006c06b31d6e4af70ed2"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
