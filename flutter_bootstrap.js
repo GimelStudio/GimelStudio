3a59f7309c13ff9cb01b1cfc708ae5ba391187dd
Correct-Syntax <correctsyntax@yahoo.com>
2025-02-28 16:39:52 +0000

    Build (092451d) Optimize setPropertyValue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2492981987"
+    serviceWorkerVersion: "2090439723"
   }
 });
