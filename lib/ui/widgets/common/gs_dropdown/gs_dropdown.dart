eba5be0b38c3f32d87ef6606142a327ae46f3b08
Noah Rahm <correctsyntax@yahoo.com>
2025-03-03 16:33:54 -0600

    Implement basic dropdown, closes #202

--- lib/ui/widgets/common/gs_dropdown/gs_dropdown.dart
+++ lib/ui/widgets/common/gs_dropdown/gs_dropdown.dart
@@ -1,23 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
-import 'gs_dropdown_model.dart';
+class GsDropdown extends StatefulWidget {
+  const GsDropdown({
+    super.key,
+    this.isEnabled = true,
+    this.width,
+    this.height,
+    required this.currentItem,
+    required this.items,
+    required this.onChange,
+  });
 
-class GsDropdown extends StackedView<GsDropdownModel> {
-  const GsDropdown({super.key});
+  final bool isEnabled;
+  final double? width;
+  final double? height;
+  final String currentItem;
+  final List<String> items;
+  final ValueChanged<String?> onChange;
 
   @override
-  Widget builder(
-    BuildContext context,
-    GsDropdownModel viewModel,
-    Widget? child,
-  ) {
-    return const SizedBox.shrink();
+  State<GsDropdown> createState() => _GsDropdownState();
+}
+
+class _GsDropdownState extends State<GsDropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<String>(
+      enabled: widget.isEnabled,
+      initialSelection: widget.currentItem,
+      width: widget.width,
+      menuHeight: widget.height,
+      enableSearch: false,
+      enableFilter: true,
+      expandedInsets: EdgeInsets.zero,
+      onSelected: widget.onChange,
+      searchCallback: (entries, query) {},
+      trailingIcon: PhosphorIcon(
+        PhosphorIcons.caretDown(PhosphorIconsStyle.light),
+        color: widget.isEnabled ? Colors.white70 : Colors.white70.withAlpha(153),
+        size: 10.0,
+      ),
+      selectedTrailingIcon: PhosphorIcon(
+        PhosphorIcons.caretUp(PhosphorIconsStyle.light),
+        color: widget.isEnabled ? Colors.white70 : Colors.white70.withAlpha(153),
+        size: 10.0,
+      ),
+      textStyle: TextStyle(
+        color: widget.isEnabled ? Colors.white70 : Colors.white70.withAlpha(153),
+        fontSize: 12.0,
+        height: 1.1,
+      ),
+      menuStyle: MenuStyle(
+        backgroundColor: WidgetStatePropertyAll(Color(0xFF1F1F1F)),
+          visualDensity: VisualDensity.compact,
+          padding: WidgetStatePropertyAll(EdgeInsets.zero),
+          surfaceTintColor: WidgetStatePropertyAll(Colors.transparent),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        suffixIconConstraints: BoxConstraints.tightFor(height: 30.0),
+        isDense: true,
+        contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.0),
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xFF363636))),
+        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xFF363636))),
+        disabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xFF363636).withAlpha(153))),
+      ),
+      dropdownMenuEntries: widget.items.map<DropdownMenuEntry<String>>((String value) {
+        return DropdownMenuEntry<String>(
+          value: value,
+          label: value,
+          style: MenuItemButton.styleFrom(
+            backgroundColor: Color(0xFF1F1F1F),
+            foregroundColor: Colors.white70,
+            textStyle: TextStyle(
+              fontSize: 12.0,
+            ),
+            visualDensity: VisualDensity(horizontal: 0.6, vertical: 0.0),
+            minimumSize: Size(70.0, 28.0),
+          ),
+        );
+      }).toList(),
+    );
   }
-
-  @override
-  GsDropdownModel viewModelBuilder(
-    BuildContext context,
-  ) =>
-      GsDropdownModel();
 }
