445fd9ba947357677f22ad92435b8de50e78096c
Correct-Syntax <correctsyntax@yahoo.com>
2025-03-04 00:28:25 +0000

    Build (46781ef) Fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "da967fad356643acf318c9066c0bd487",
 "manifest.json": "075a4a02f9b9da38f5f0f2ebaf17ad7f",
 "favicon.png": "85090bb4c150a1101805df086e708563",
-"flutter_bootstrap.js": "01de9e306b587d38c3636a3b897d10f8"};
+"flutter_bootstrap.js": "0ad37d923f671f358833163b8a011c22"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
