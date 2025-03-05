8958358203f14e42093f00b64fc45cba872e0411
Correct-Syntax <correctsyntax@yahoo.com>
2025-03-05 02:04:02 +0000

    Build (c2d3bcf) Dynamically scale overlay handled based on viewport scale

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "318626226"
+    serviceWorkerVersion: "3988423893"
   }
 });
