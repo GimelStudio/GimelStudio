a9f856bf8987d36a257eb67160ebd07da8f6477b
Correct-Syntax <correctsyntax@yahoo.com>
2025-03-05 05:48:39 +0000

    Build (c3fad21) Work on new document dialog - Recents and open file are not yet implemented

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "148169838"
+    serviceWorkerVersion: "3189621677"
   }
 });
