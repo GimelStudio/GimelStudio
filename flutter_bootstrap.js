2932a862e45f4784a2f3db21375f7945bb53164b
Correct-Syntax <correctsyntax@yahoo.com>
2025-03-05 02:13:59 +0000

    Build (1927951) Viewport improvements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3988423893"
+    serviceWorkerVersion: "148169838"
   }
 });
