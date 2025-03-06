9e74897fb056f2f69f479fa5e6c6124afaf45c84
Correct-Syntax <correctsyntax@yahoo.com>
2025-03-06 22:30:15 +0000

    Build (8db0386) Add env variables

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2395162942"
+    serviceWorkerVersion: "2315678561"
   }
 });
