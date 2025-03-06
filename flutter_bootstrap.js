96baca3c556f5a755a73d7ad591270b8ebd8bcd8
Correct-Syntax <correctsyntax@yahoo.com>
2025-03-06 02:47:28 +0000

    Build (0738b76) Improve tooltip widget

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1437498902"
+    serviceWorkerVersion: "4093344058"
   }
 });
