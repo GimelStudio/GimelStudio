92124ca66e80f0827fdda1a9ffefe6d128c88590
Noah Rahm <correctsyntax@yahoo.com>
2025-03-06 16:28:14 -0600

    Implement about dialog

--- lib/ui/dialogs/about/about_dialog.dart
+++ lib/ui/dialogs/about/about_dialog.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gimelstudio/ui/widgets/common/gs_flat_icon_btn/gs_flat_icon_btn.dart';
+import 'package:gimelstudio/ui/widgets/common/gs_tooltip/gs_tooltip.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -9,10 +12,10 @@ class AboutDialog extends StackedView<AboutDialogModel> {
   final Function(DialogResponse) completer;
 
   const AboutDialog({
-    Key? key,
+    super.key,
     required this.request,
     required this.completer,
-  }) : super(key: key);
+  });
 
   @override
   Widget builder(
@@ -21,27 +24,226 @@ class AboutDialog extends StackedView<AboutDialogModel> {
     Widget? child,
   ) {
     return AlertDialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
       backgroundColor: Color(0xFF191919),
       contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
       content: ConstrainedBox(
-        constraints: const BoxConstraints(minWidth: 400.0, maxWidth: 400.0),
+        constraints: const BoxConstraints(minWidth: 500.0, maxWidth: 500.0, maxHeight: 300.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: [
-            InkWell(
-              onTap: () {
-                completer(DialogResponse(confirmed: true));
-              },
-              child: const Text(
-                'Close',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14.0,
+            // Close button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GsTooltip(
+                  text: 'Close',
+                  position: TooltipPosition.bottom,
+                  child: InkWell(
+                    onTap: () {
+                      completer(DialogResponse(confirmed: true));
+                    },
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PhosphorIcon(
+                        PhosphorIcons.x(PhosphorIconsStyle.regular),
+                        size: 18.0,
+                        color: Colors.white54,
+                      ),
+                    ),
+                  ),
                 ),
+              ],
+            ),
+
+            // Main content
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // Logo icon
+                            Image.asset(
+                              'assets/gimel-studio-icon.png',
+                              width: 80.0,
+                              height: 80.0,
+                              isAntiAlias: true,
+                              cacheHeight: 170,
+                              cacheWidth: 170,
+                              filterQuality: FilterQuality.medium,
+                            ),
+                            const SizedBox(width: 14.0),
+                            // Version
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Gimel Studio',
+                                  style: TextStyle(
+                                    height: 1.3,
+                                    color: Colors.white,
+                                    fontSize: 33.0,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: -0.01,
+                                  ),
+                                ),
+                                Text(
+                                  'Version ${viewModel.getApplicationVersion()} ${viewModel.platform == '' ? '' : 'for ${viewModel.platform}'}',
+                                  style: TextStyle(
+                                    height: 1.0,
+                                    color: Colors.white,
+                                    fontSize: 13.0,
+                                    fontWeight: FontWeight.w500,
+                                    letterSpacing: -0.01,
+                                  ),
+                                ),
+                                // Build information
+                                Text(
+                                  'Built on ${viewModel.getBuildDate()} from branch ${viewModel.buildBranch}, commit ${viewModel.buildCommit}.',
+                                  style: TextStyle(
+                                    height: 2.5,
+                                    color: Colors.white54,
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            // Copy version info button
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                GsTooltip(
+                                  text: 'Copy application info to the clipboard',
+                                  position: TooltipPosition.bottom,
+                                  child: InkWell(
+                                    onTap: viewModel.copyInfoToClipBoard,
+                                    borderRadius: BorderRadius.circular(20.0),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        spacing: viewModel.infoCopied ? 4.0 : 0.0,
+                                        children: [
+                                          PhosphorIcon(
+                                            PhosphorIcons.copy(PhosphorIconsStyle.regular),
+                                            size: 16.0,
+                                            color: Colors.white54,
+                                          ),
+                                          if (viewModel.infoCopied)
+                                            Text(
+                                              'Copied!',
+                                              style: TextStyle(color: Colors.white54, fontSize: 11.0),
+                                            ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+
+                        // Buttons
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: Row(
+                              spacing: 18.0,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                // First column
+                                Expanded(
+                                  child: Column(
+                                    spacing: 4.0,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      GsFlatIconBtn(
+                                        label: 'Contribute',
+                                        icon: PhosphorIcon(
+                                          PhosphorIcons.smiley(PhosphorIconsStyle.light),
+                                          color: Colors.white,
+                                          size: 18.0,
+                                        ),
+                                        onTap: viewModel.onContribute,
+                                      ),
+                                      GsFlatIconBtn(
+                                        label: 'Visit Website',
+                                        icon: PhosphorIcon(
+                                          PhosphorIcons.globeSimple(PhosphorIconsStyle.light),
+                                          color: Colors.white,
+                                          size: 18.0,
+                                        ),
+                                        onTap: viewModel.onVisitWebsite,
+                                      ),
+                                      GsFlatIconBtn(
+                                        label: 'Join Community Chat',
+                                        icon: PhosphorIcon(
+                                          PhosphorIcons.chats(PhosphorIconsStyle.light),
+                                          color: Colors.white,
+                                          size: 18.0,
+                                        ),
+                                        onTap: viewModel.onJoinCommunityChat,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+
+                                // Second column
+                                Expanded(
+                                  child: Column(
+                                    spacing: 4.0,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      GsFlatIconBtn(
+                                        label: 'Credits',
+                                        icon: PhosphorIcon(
+                                          PhosphorIcons.article(PhosphorIconsStyle.light),
+                                          color: Colors.white,
+                                          size: 18.0,
+                                        ),
+                                        onTap: viewModel.onCredits,
+                                      ),
+                                      GsFlatIconBtn(
+                                        label: 'Licenses',
+                                        icon: PhosphorIcon(
+                                          PhosphorIcons.fileText(PhosphorIconsStyle.light),
+                                          color: Colors.white,
+                                          size: 18.0,
+                                        ),
+                                        onTap: viewModel.onLicenses,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
+
+            // Bottom text
+            Text(
+              'Gimel Studio is free software licensed under the terms of the GPL-3.0 license.\n© 2025 Gimel Studio contributors.',
+              style: TextStyle(
+                color: Colors.white54,
+                fontSize: 11.0,
+              ),
+            )
           ],
         ),
       ),
--- lib/ui/dialogs/about/about_dialog_model.dart
+++ lib/ui/dialogs/about/about_dialog_model.dart
@@ -1,3 +1,54 @@
+import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class AboutDialogModel extends BaseViewModel {}
+class AboutDialogModel extends BaseViewModel {
+  String platform = String.fromEnvironment('PLATFORM', defaultValue: '');
+  String buildDateTime = String.fromEnvironment('BUILD_DATETIME', defaultValue: '');
+  String buildBranch = String.fromEnvironment('BUILD_BRANCH', defaultValue: '(unknown)');
+  String buildCommit = String.fromEnvironment('BUILD_COMMIT', defaultValue: '(unknown)');
+
+  bool infoCopied = false;
+
+  String getApplicationVersion() {
+    // TODO
+    return '0.7.0-alpha';
+  }
+
+  String getBuildDate() {
+    if (buildDateTime == '') {
+      return '(unknown)';
+    }
+    DateTime datetime = DateTime.parse(buildDateTime);
+    return datetime.toIso8601String().split('T')[0];
+  }
+
+  Future<void> copyInfoToClipBoard() async {
+    String applicationInfo =
+        'Gimel Studio v${getApplicationVersion()} for $platform built on ${getBuildDate()} from branch $buildBranch, commit $buildCommit.';
+    await Clipboard.setData(ClipboardData(text: applicationInfo));
+
+    infoCopied = true;
+    rebuildUi();
+  }
+
+  Future<void> onContribute() async {
+    await launchUrl(Uri.https('github.com', '/GimelStudio/GimelStudio'));
+  }
+
+  Future<void> onVisitWebsite() async {
+    await launchUrl(Uri.https('gimelstudio.com'));
+  }
+
+  Future<void> onJoinCommunityChat() async {
+    await launchUrl(Uri.https('gimelstudio.zulipchat.com', '/join/sif32f3gjpnikveonzgc7zhw/'));
+  }
+
+  Future<void> onCredits() async {
+    await launchUrl(Uri.https('github.com', '/GimelStudio/GimelStudio/blob/master/CONTRIBUTORS.md'));
+  }
+
+  Future<void> onLicenses() async {
+    await launchUrl(Uri.https('github.com', '/GimelStudio/GimelStudio/blob/master/LICENSE'));
+  }
+}
