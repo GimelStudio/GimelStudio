07ad407f3d59e85952e6b5bbae5e7a85a2d983bb
Correct-Syntax <correctsyntax@yahoo.com>
2025-03-24 17:16:26 +0000

    Build (6dc9668) Bump image from 4.5.3 to 4.5.4 (#217)
    Bumps [image](https://github.com/brendan-duncan/image) from 4.5.3 to 4.5.4.
    - [Changelog](https://github.com/brendan-duncan/image/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/brendan-duncan/image/commits)
    
    ---
    updated-dependencies:
    - dependency-name: image
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1365949123"
+    serviceWorkerVersion: "1876750158"
   }
 });
