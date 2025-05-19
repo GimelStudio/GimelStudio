869a767299b4be2720d562fb35af939d0e2101f3
Correct-Syntax <correctsyntax@yahoo.com>
2025-05-19 15:33:26 +0000

    Build (72e3f2f) Bump file_picker from 10.1.7 to 10.1.9 (#224)
    Bumps [file_picker](https://github.com/miguelpruivo/flutter_file_picker) from 10.1.7 to 10.1.9.
    - [Release notes](https://github.com/miguelpruivo/flutter_file_picker/releases)
    - [Changelog](https://github.com/miguelpruivo/flutter_file_picker/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/miguelpruivo/flutter_file_picker/commits)
    
    ---
    updated-dependencies:
    - dependency-name: file_picker
      dependency-version: 10.1.9
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3145846320"
+    serviceWorkerVersion: "4168606156"
   }
 });
