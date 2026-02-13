a697d30f4ad4fa13167edc6822083fc24d3ac29e
Correct-Syntax <correctsyntax@yahoo.com>
2026-02-13 01:30:07 +0000

    Build (d2323c2) Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4263900482" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3738997323" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
