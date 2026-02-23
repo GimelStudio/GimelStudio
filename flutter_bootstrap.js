a4aeb9aabcffabdf641d3434cdcbad6a6a2015f0
Correct-Syntax <correctsyntax@yahoo.com>
2026-02-23 17:28:08 +0000

    Build (d5dad5e) Bump image from 4.7.2 to 4.8.0 (#234)
    Bumps [image](https://github.com/brendan-duncan/image) from 4.7.2 to 4.8.0.
    - [Changelog](https://github.com/brendan-duncan/image/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/brendan-duncan/image/commits/v4.8.0)
    
    ---
    updated-dependencies:
    - dependency-name: image
      dependency-version: 4.8.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1837713067" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3209713858" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
