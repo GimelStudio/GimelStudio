76276255e98f5c04e3d7726936d036d234ffec39
Correct-Syntax <correctsyntax@yahoo.com>
2026-05-25 21:24:18 +0000

    Build (697e5ae) Delete .github/dependabot.yml

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "93822902" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1859281379" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
